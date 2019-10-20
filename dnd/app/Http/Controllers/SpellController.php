11f3e6e539c89e20746fa2400ba66f06a0367f4d
Ashkan Abedian <38367785+asabedia@users.noreply.github.com>
2019-10-19 22:44:12 -0400

    adding functionality to read from csv

--- dnd/app/Http/Controllers/SpellController.php
+++ dnd/app/Http/Controllers/SpellController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 use App\Spell;
 
+use League\Csv\Reader;
+
 class SpellController extends Controller
 {
     /**
@@ -10,9 +12,28 @@ class SpellController extends Controller
      *
      * @return void
      */
+
+    private function getSpellsFromCsv($csv_path) {
+        $reader = Reader::createFromPath($csv_path, 'r');
+        $keys = ["name", "level", "school", "ritual",
+         "casting_time", "range",	"duration","concentration",
+         "components", "materials", "description_length",
+         "classes"];
+        $spells = array();
+        $spell_from_csv = $reader->fetchAssoc($keys);
+        foreach ($spell_from_csv as $row) {
+            $spell = new Spell;
+            foreach ($keys as $attr) {
+                $spell->$attr=$row[$attr];
+            }
+            array_push($spells, $spell);
+        }
+        return $spells;
+    }
+
     public function index()
     {
-        $spells = Spell::all();
-        return view('spells', ['spells' => $spells]);
+        $spells = $this->getSpellsFromCsv("C:/wamp64/www/2019-team10/dnd/resources/csv/spell_csv.csv");
+        return view('spells', ['spells' => $spells, 'attr_to_display' => ["name", "level", "school", "classes"]]);
     }
 }
