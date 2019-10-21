4a0db4672f6a25e34d7766a8ce3b0dcc919333e9
Moe <MoeCheikh@users.noreply.github.com>
2019-10-20 21:40:11 -0400

    Aesthetics
    a e s t h e t i c

--- dnd/resources/views/addnewspells.blade.php
+++ dnd/resources/views/addnewspells.blade.php
@@ -3,47 +3,81 @@
 @section('title', 'Page Title')
 
 @section('content')
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+  <style>
+body {
+	background-image: url("https://images.squarespace-cdn.com/content/v1/51b3dc8ee4b051b96ceb10de/1558559745443-KM38DVM6H0AIJWVJNT1H/ke17ZwdGBToddI8pDm48kJe8VwonRcYgr7f_0UVbdhh7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UbHrcextDeErdIU23wx0_6BTOY9zQNi_nItQjMEsHFYhlvkRmRO1_mFZFNCn67QdSw/ghosts_saltmarsh.jpg?format=2500w ");
+        background-repeat:no-repeat;
+       background-size:cover;
+} 
+.transparentbox{
+ color: #fff;
+}
+</style>
+</head>
+
+
+<body>
+
+<div class="col-sm-10">
+<div style="background:#DCDCDC" class="jumbotron text-center">
+  <font color="#8B0000"><h1>Add New Spell Page</h1></font>
+</div>
+</div>
+
+<div class = transparentbox>
+
 <form>
 	
   <div class="form-group row">
     <div class="col-sm-10">
-	<label for="spellname" class="col-sm-2 col-form-label">Spell Name: </label>
+	<label> <b>Spell Name:</b> </label>
       <input type="spellname" class="form-control" id="spellname" placeholder="Spell Name">
+    </div>
+	    <div class="col-sm-10">
+	<label> <b>Level:</b> </label>
+      <input type="spellname" class="form-control" id="level" placeholder="Level">
     </div>
 	</div>
 	  <div class="form-group row">
     <div class="col-sm-10">
-	<label for="type" class="col-sm-2 col-form-label">Type: </label>
+	<label> <b>Type:</b> </label>
       <input type="type" class="form-control" id="type" placeholder="Type">
     </div>
 	</div>
 		  <div class="form-group row">
     <div class="col-sm-10">
-	<label for="castingtime" class="col-sm-2 col-form-label">Casting Time: </label>
+	<label> <b>Casting Time:</b> </label>
       <input type="castingtime" class="form-control" id="castingtime" placeholder="Casting Time">
     </div>
 	</div>
 		  <div class="form-group row">
     <div class="col-sm-10">
-	<label for="components" class="col-sm-2 col-form-label">Components: </label>
+	<label> <b>Components:</b> </label>
       <input type="components" class="form-control" id="components" placeholder="Components">
     </div>
 	</div>
 		  <div class="form-group row">
     <div class="col-sm-10">
-	<label for="duration" class="col-sm-2 col-form-label">Duration: </label>
+	<label> <b>Duration:</b> </label>
       <input type="duration" class="form-control" id="duration" placeholder="Duration">
     </div>
 	</div>
 		  <div class="form-group row">
     <div class="col-sm-10">
-	<label for="rangey" class="col-sm-2 col-form-label">Range: </label>
+	<label> <b>Range</b> </label>
       <input type="rangey" class="form-control" id="rangey" placeholder="Range">
     </div>
 	</div>
 		  <div class="form-group row">
     <div class="col-sm-10">
-	<label for="description" class="col-sm-2 col-form-label">Description: </label>
+	<label> <b>Description:</b> </label>
       <input type="description" class="form-control" id="description" placeholder="Description">
     </div>
 	</div>
@@ -51,19 +85,20 @@
 	
 
 
-  <fieldset class="form-group">
+    <fieldset class="form-group">
     <div class="row">
-      <legend class="col-form-label col-sm-2 pt-0">Ritual/NonRitual</legend>
+	
       <div class="col-sm-10">
+	  <label> <b>Non/Ritual:</b> </label>
         <div class="form-check">
-          <input class="form-check-input" type="radio" name="grid1" id="gridRadios1" value="option1" checked>
-          <label class="form-check-label" for="gridRadios1">
+          <input class="form-check-input" type="radio" name="grid1" id="select1" value="option3" checked>
+          <label class="form-check-label" for="gridRadios3">
             Ritual
           </label>
         </div>
         <div class="form-check">
-          <input class="form-check-input" type="radio" name="grid1" id="gridRadios2" value="option2">
-          <label class="form-check-label" for="gridRadios2">
+          <input class="form-check-input" type="radio" name="grid1" id="select2" value="option4">
+          <label class="form-check-label" for="gridRadios4">
             Non-Ritual
           </label>
         </div>
@@ -71,10 +106,12 @@
       </div>
     </div>
   </fieldset>
+  
     <fieldset class="form-group">
     <div class="row">
-      <legend class="col-form-label col-sm-2 pt-0">Concentration/Non-Concentration</legend>
+	
       <div class="col-sm-10">
+	  <label> <b>Non/Concentration:</b> </label>
         <div class="form-check">
           <input class="form-check-input" type="radio" name="grid2" id="select1" value="option3" checked>
           <label class="form-check-label" for="gridRadios3">
@@ -93,23 +130,25 @@
   </fieldset>
   
   <div class="form-group row">
-    <div class="col-sm-2">Select Classes (all that apply)</div>
+	
     <div class="col-sm-10">
-	@foreach ($classes as $class)
-      <div class="form-check">
-        <input class="form-check-input" type="checkbox" id="gridCheck1">
-        <label class="form-check-label" for="gridCheck1">
-		{{$class}}
-        </label>
-      </div>
+	<label> <b>Select All Classes That Apply:</b> </label>
+	<br>@foreach ($classes as $class)
+	<label for="inlineCheckbox1"> {{$class }}   </label>
+	   <div class="form-check form-check-inline">
+                        <input type="checkbox" class="styled" id="inlineCheckbox1" value="option1">
+                        
+                    </div>
 	 @endforeach
     </div>
-
   </div>
+       
   <div class="form-group row">
-    <div class="col-sm-10">
-      <button type="submit" class="btn btn-primary">ADD NEW SPELL</button>
+    <div class="col-sm-10" align="center">
+      <button type="submit" class="btn btn-danger"><font color="white">ADD A SPELL</font></button>
     </div>
   </div>
+  </body>
 </form>
+</div>
 @endsection
