7ccbe27c16c625f3591806d813066955b060b679
Raja-S117 <55563537+Raja-S117@users.noreply.github.com>
2019-10-20 21:29:34 -0400

    Update spells.blade.php

--- dnd/resources/views/spells.blade.php
+++ dnd/resources/views/spells.blade.php
@@ -3,22 +3,100 @@
 @section('title', 'Page Title')
 
 @section('content')
+
 <html>
-<div style="margin: 20px; padding: 20px; height: 100px; width: 50%; text-align: center; background-color: #3D3131; border: 10px solid black">
+
+<div style="margin: 20px; display: inline-block; padding: 20px; height: 100px; width: 50%;
+ text-align: center; background-color: #3D3131; border: 10px solid black">
 <h1 align = "center"><font size = "15"; color = #D30909> Dungeons & Dragons</font></h1>
 </div>
 
 
 <body style="background-image:url(https://wallpaperaccess.com/full/117898.jpg)">
 <h2> </h2>
-<table style = "width:100%">
+
+
+<div style="border:3px solid black; height:200px;overflow:auto;">
+<table bgcolor = #B9C3BD style = "width:100%;">
 <tr>
-    <th>Level</th>
-    <th>Name</th>
-    <th>Class</th>
-    <th>Component</th>
-    <th>School</th>
+    <th align = "left">Level</th>
+    <th align = "left">Name</th>
+    <th align = "left">Class</th>
+    <th align = "left">Component</th>
+    <th align = "left">School</th>
 </tr>
+</tr>
+    <td>5</td>
+    <td>Hellfire</td>
+    <td>Druid</td>
+    <td>Earth</td>
+    <td>Waterloo</td>
+</tr>
+</tr>
+    <td>8</td>
+    <td>Frost Bite</td>
+    <td>Elf</td>
+    <td>Fire</td>
+    <td>Laurier</td>
+</tr>
+</tr>
+    <td>10</td>
+    <td>Claymore</td>
+    <td>Dward</td>
+    <td>Dynamite</td>
+    <td>Rivendel</td>
+</tr>
+</tr>
+    <td>10</td>
+    <td>Claymore</td>
+    <td>Dward</td>
+    <td>Dynamite</td>
+    <td>Rivendel</td>
+</tr>
+</tr>
+    <td>10</td>
+    <td>Claymore</td>
+    <td>Dward</td>
+    <td>Dynamite</td>
+    <td>Rivendel</td>
+</tr>
+</tr>
+    <td>10</td>
+    <td>Claymore</td>
+    <td>Dward</td>
+    <td>Dynamite</td>
+    <td>Rivendel</td>
+</tr>
+</tr>
+    <td>10</td>
+    <td>Claymore</td>
+    <td>Dward</td>
+    <td>Dynamite</td>
+    <td>Rivendel</td>
+</tr>
+</tr>
+    <td>10</td>
+    <td>Claymore</td>
+    <td>Dward</td>
+    <td>Dynamite</td>
+    <td>Rivendel</td>
+</tr>
+</tr>
+    <td>10</td>
+    <td>Claymore</td>
+    <td>Dward</td>
+    <td>Dynamite</td>
+    <td>Rivendel</td>
+</tr>
+</tr>
+    <td>10</td>
+    <td>Claymore</td>
+    <td>Dward</td>
+    <td>Dynamite</td>
+    <td>Rivendel</td>
+</tr>
+</div>
+
 </body>
 </html>
 @endsection
