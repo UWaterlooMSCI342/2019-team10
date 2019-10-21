2b0c364c3a1cd2578ee95f4f95d1a502bd9699a8
Raja-S117 <55563537+Raja-S117@users.noreply.github.com>
2019-10-20 20:45:06 -0400

    Update spells.blade.php

--- dnd/resources/views/spells.blade.php
+++ dnd/resources/views/spells.blade.php
@@ -3,7 +3,14 @@
 @section('title', 'Page Title')
 
 @section('content')
+<html>
+<div style="margin: 20px; padding: 20px; height: 100px; width: 50%; text-align: center; background-color: #3D3131; border: 10px solid black">
+<h1 align = "center"><font size = "15"; color = #D30909> Dungeons & Dragons</font></h1>
+</div>
 
+
+<body style="background-image:url(https://wallpaperaccess.com/full/117898.jpg)">
+<h2> </h2>
 <table style = "width:100%">
 <tr>
     <th>Level</th>
@@ -12,6 +19,6 @@
     <th>Component</th>
     <th>School</th>
 </tr>
-
-
+</body>
+</html>
 @endsection
