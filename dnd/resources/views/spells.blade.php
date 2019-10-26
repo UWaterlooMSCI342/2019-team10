9855873bc531d24b66366c1584d2ca3b6fc84fc3
Raja-S117 <55563537+Raja-S117@users.noreply.github.com>
2019-10-26 18:55:24 -0400

    drop down lists

--- dnd/resources/views/spells.blade.php
+++ dnd/resources/views/spells.blade.php
@@ -4,45 +4,60 @@
 
 @section('content')
 <html>
-
+<body style="background-image:url(https://wallpaperaccess.com/full/117898.jpg)"> 
 <div style="margin: 20px; display: inline-block; padding: 20px; height: 90px; width: 30%;
  text-align: center; background-color: #3D3131; border: 10px solid black;">
     <h1 align = "center"><font size = "5"; color = #D30909> Dungeons & Dragons</font></h1>
 </div>
 
-<div class="container">                                         
-  <div class="dropdown">
-    <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Filter By
-    </button>
-    <ul class="dropdown-menu">
-      <li><a href="#">Level</a></li>
-      <li class = "divider"></li>
-      <li><a href="#">Name</a></li>
-      <li class = "divider"></li>
-      <li><a href="#">Class</a></li>
-      <li class = "divider"></li>
-      <li><a href="#">Component</a></li>
-      <li class = "divider"></li>
-      <li class = "dropdown submenu"><a href="#">School <span class="caret"></span></a>
-        <ul class = "dropdown-menu">
-        <li><a href="#">Test</a></li>
-        </ul>
-      </li>
-    </ul>
-  </div>
+<div >
+    <div class="btn-group">                                         
+        <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Level
+        <span class = "caret"></span></button>
+        <div class="dropdown-menu">
+        <a class="dropdown-item" href="#">Level</span></a>
+        </div>
+    </div>
+    <div class="btn-group">                                         
+        <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Name
+        <span class = "caret"></span></button>
+        <div class="dropdown-menu">
+        <a class="dropdown-item" href="#">Name</span></a>
+        </div>
+    </div>
+    <div class="btn-group">                                         
+        <button type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Class
+        <span class = "caret"></span></button>
+        <div class="dropdown-menu">
+        <a class="dropdown-item" href="#">Class</span></a>
+        </div>
+    </div>
+    <div class="btn-group">                                         
+        <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Component
+        <span class = "caret"></span></button>
+        <div class="dropdown-menu">
+        <a class="dropdown-item" href="#">Component</span></a>
+        </div>
+    </div>
+    <div class="btn-group">                                         
+        <button type="button" class="btn btn-warning dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">School
+        <span class = "caret"></span></button>
+        <div class="dropdown-menu">
+        <a class="dropdown-item" href="#">School</span></a>
+        </div>
+    </div>
 </div>
-<h2> </h2>
-
+<br/>
 <div style="border:3px solid black; height:400px;overflow:auto;">
 
-    <table class = "table table-light table-striped" style = "width:100%;">
-        <thead class = "thead thead-dark">
+    <table class = "table table-dark">
+        <thead>
             <tr>
-                <th align = "left">Level</th>
-                <th align = "left">Name</th>
-                <th align = "left">Class</th>
-                <th align = "left">Component</th>
-                <th align = "left">School</th>
+                <th scope = "col">Level</th>
+                <th scope = "col">Name</th>
+                <th scope = "col">Class</th>
+                <th scope = "col">Component</th>
+                <th scope = "col">School</th>
             </tr>
         </thead>
         <tbody>
@@ -58,15 +73,6 @@
         </tbody>
     </table>
 </div>
-
-<script>
-$(document).ready(function(){
-  $('.dropdown-submenu a.test').on("click", function(e){
-    $(this).next('ul').toggle();
-    e.stopPropagation();
-    e.preventDefault();
-  });
-});
-</script>
+</body>
 </html>
 @endsection
