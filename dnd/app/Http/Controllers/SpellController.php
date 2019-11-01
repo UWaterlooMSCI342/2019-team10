c98ffd4b0b611d8412aa7ee7045e7f42abadfe21
Anna <annajamtorki@gmail.com>
2019-11-01 19:58:19 -0400

    In progress NewSave function

--- dnd/app/Http/Controllers/SpellController.php
+++ dnd/app/Http/Controllers/SpellController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 use App\Spell;
 use App\SpellClass;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\DB;
+use attribute;
 use League\Csv\Reader;
 
 class SpellController extends Controller
@@ -34,36 +35,48 @@ class SpellController extends Controller
       }
     public function NewSave(Request $request){
         $spell = new Spell;
-        $spell->attribute('spellname');
-        $spell->attribute('level'); 
-        $spell->attribute('type'); 
-        $spell->attribute('castingtime'); 
-        $spell->attribute('components'); 
-        $spell->attribute('duration'); 
-        $spell->attribute('range'); 
-        $spell->attribute('description'); 
-        $spell->attribute('ritual'); 
-        $spell->attribute('concentration'); 
-        $spell->attribute('classes'); 
+        $spell->name=spellname;
+        $spell->level='level'; 
+        $spell->school='type'; 
+        $spell->casting_time='castingtime'; 
+        $spell->components='components'; 
+        $spell->duration='duration'; 
+        $spell->range='range'; 
+        $spell->description='description'; 
+        $spell->ritual='ritual'; 
+        $spell->concentration='concentration'; 
+        //$class->classes='classes'; 
+        $spell->save();
         $request->input('spellname', 'level', 'type', 'castingtime', 'components', 'duration','range', 'description', 'ritual', 'concentration', 'classes');
+        // DB::insert('insert into users (spellname, NEXTCOLUMNTABLE) values (?, ?)', [$resquest->input('spellname'), NEXTREQUEST]);
+        // DB::insert('insert into users (level, NEXTCOLUMNTABLE) values (?, ?)', [$resquest->input('level',null), NEXTREQUEST]);
+        // DB::insert('insert into users (type, NEXTCOLUMNTABLE) values (?, ?)', [$resquest->input('type'), NEXTREQUEST]);
+        // DB::insert('insert into users (castingtime, NEXTCOLUMNTABLE) values (?, ?)', [$resquest->input('castingtime'), NEXTREQUEST]);
+        // DB::insert('insert into users (components, NEXTCOLUMNTABLE) values (?, ?)', [$resquest->input('components'), NEXTREQUEST]);
+        // DB::insert('insert into users (duration, NEXTCOLUMNTABLE) values (?, ?)', [$resquest->input('duration'), NEXTREQUEST]);
+        // DB::insert('insert into users (range, NEXTCOLUMNTABLE) values (?, ?)', [$resquest->input('range'), NEXTREQUEST]);
+        // DB::insert('insert into users (description, NEXTCOLUMNTABLE) values (?, ?)', [$resquest->input('description'), NEXTREQUEST]);
+        // DB::insert('insert into users (ritual, NEXTCOLUMNTABLE) values (?, ?)', [$resquest->input('ritual'), NEXTREQUEST]);
+        // DB::insert('insert into users (concentration, NEXTCOLUMNTABLE) values (?, ?)', [$resquest->input('concentration'), NEXTREQUEST]);
+        //DB::insert('insert into users (classes, NEXTCOLUMNTABLE) values (?, ?)', [$resquest->input('classes'), NEXTREQUEST]);
         $spell = Spell::create();
-        $spell -> classes()->attach('api/spell');
-        foreach ($spell as $attr) {
-            if ($attr == 'classes') {
-                $tmp=explode (",", $row[$attr]);
-                foreach ($tmp as $class) {
-                    $class = SpellClass::firstOrCreate(['class_name'=>$class]);
-                    array_push($spell_classes, $class);
-                }
-            } else {
-                $spell->$attr=$row[$attr];
-            }
-        }
-            $spell->save();
-            foreach($spell_classes as $class) {
-                $spell->classes()->attach($class, ['name' => $spell->name, 'class_name' => $class->class_name]);
-            }
-        //$spell->belongsToMany(Spell::class);
+        $spell -> classes($class)->attach('api/spell');
+        // foreach ($spell as $attr) {
+        //     if ($attr == 'classes') {
+        //         $tmp=explode (",", $row[$attr]);
+        //         foreach ($tmp as $class) {
+        //             $class = SpellClass::firstOrCreate(['class_name'=>$class]);
+        //             array_push($spell_classes, $class);
+        //         }
+        //     } else {
+        //         $spell->$attr=$row[$attr];
+        //     }
+        // }
+        //     $spell->save();
+        //     foreach($spell_classes as $class) {
+        //         $spell->classes()->attach($class, ['name' => $spell->name, 'class_name' => $class->class_name]);
+        //     }
+        $spell->ManyToMany(Spell::class);
         return redirect(url("api/spells"));
     }
 }
