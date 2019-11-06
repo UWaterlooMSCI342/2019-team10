531075ac9cfaa939b1da338a0a710bbe5035d1cb
Moe <MoeCheikh@users.noreply.github.com>
2019-11-06 14:34:45 -0500

    Changes to add more filtering options for user (any option for each)

--- dnd/resources/views/spells.blade.php
+++ dnd/resources/views/spells.blade.php
@@ -10,11 +10,12 @@
 </div>
 
 <div>
-    <div class="btn-group">                                         
-        <a href="{{url('/api/add')}}" class="btn btn-primary">Add Spell</a>
-    </div>
+
 	
     <div class="btn-group">                                         
+        <a href="{{url('/api/add')}}" class="btn btn-primary">Add Spell</a>
+        </div>
+<div class="btn-group">                                         
         <button type="button" class="btn dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Level
         <span class = "caret"></span></button>
         <div class="dropdown-menu">
@@ -69,13 +70,6 @@
 <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#Modal">
   Advanced Filter
 </button>
-
-
-<div class="btn-group">                                         
-        <a href="{{url('/api/spellbooks')}}" class="btn btn-danger">View Spellbooks</a>
-  </div>
-
-
 </div>
 
 <div class="modal fade" id="Modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
@@ -89,28 +83,37 @@
       </div>
       <div class="modal-body">
         <form action = "{{url('/api/spell/filter/multifilter')}}" method = "POST">	
+<label><b>Level:</b></label>
 <select name = "level" class = "browser-default custom-select custom-select-lg mb-3">
+		<option selected value = "Any"> -- Any -- </option>
 		@foreach($levels as $level) 
 		<option value = "{{($level->level)}}">{{($level->level)}}</option>
 		@endforeach
     </select>
-
+<label><b>Class:</b></label>
  <select name = "class" class = "browser-default custom-select custom-select-lg mb-3">
+		<option selected value = "Any"> -- Any -- </option>
 		@foreach($classes as $class) 
 		<option value = "{{($class->class_id)}}">{{($class->class_name)}}</option>
 		@endforeach
     </select>
+<label><b>Ritual:</b></label>
 	<select name = "ritual" class = "browser-default custom-select custom-select-lg mb-3">
+	<option selected value = "Any"> -- Any -- </option>
 		@foreach($rituals as $ritual) 
 		<option value = "{{($ritual->ritual)}}">{{($ritual->ritual)}}</option>
 		@endforeach
     </select>
+<label><b>Concentration:</b></label>
 	<select name = "concentration" class = "browser-default custom-select custom-select-lg mb-3">
+	<option selected value = "Any"> -- Any -- </option>
 		@foreach($concentrations as $concentration) 
 		<option value = "{{($concentration->concentration)}}">{{($concentration->concentration)}}</option>
 		@endforeach
     </select>
+<label><b>School:</b></label>
 	<select name = "school" class = "browser-default custom-select custom-select-lg mb-3">
+	<option selected value = "Any"> -- Any -- </option>
 		@foreach($schools as $school) 
 		<option value = "{{($school->school)}}">{{($school->school)}}</option>
 		@endforeach
@@ -119,55 +122,47 @@
       </div>
       <div class="modal-footer">
 	  <input type="submit" value="Submit" class="btn btn-success">
-      <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+      <button type="button" class="btn btn-danger" data-dismiss="modal" href="{{url('/api/spells')}}">Close</button>
       </div>
 </form>
     </div>
   </div>
 </div>
+    
+
 
 <br/>
-<form action="{{url('/api/spellbook/add')}}" method="POST">
-    <div class="btn-group wrapper" style="text-align:right, position: absolute;">                                         
-        <a style = "color: white;"class="btn btn-success btn-large" data-toggle="modal" data-target="#spellbookModal"> Add to Spellbook </a>
-    </div>
+<div style="border:3px solid black; height:400px;overflow:auto;">
+    <table class="table table-inverse table-dark">
+        <thead>
+            <tr>
+                <th scope = "col">Level</th>
+                <th scope = "col">Name</th>
+                <th scope = "col">Class</th>
+                <th scope = "col">Ritual</th>
+				<th scope = "col">Concentration</th>
+                <th scope = "col">School</th>
+            </tr>
+        </thead>
+        <tbody>
 
-    <div style="height:400px;overflow:auto;">
-        <table class="table table-inverse table-dark">
-            <thead>
+            @foreach($spells as $spell)
                 <tr>
-
-                    <th scope = "col">Level</th>
-                    <th scope = "col">Name</th>
-                    <th scope = "col">Class</th>
-                    <th scope = "col">Component</th>
-                    <th scope = "col">School</th>
-                    <th scope = "col">Delete</th>
-                    <th scope = "col">Add to Spellbook</th>
-
+                    <td>{{$spell -> level}}</td>
+                    <td><a href="{{url('/api/spell/detail/' . $spell -> spell_id)}}">{{$spell -> name}}</a></td>
+                    <td>{{$spell -> formattedClasses()}}</td>
+                    <td>{{$spell -> ritual}}</td>
+					<td>{{$spell -> concentration}}</td>
+                    <td>{{$spell -> school}}</td>
+                    <td><a href="{{url('/api/spell/' . $spell -> spell_id)}}" class = "btn">Delete</a></td>
                 </tr>
-            </thead>
-            <tbody>
+            @endforeach
+        </tbody>
+		
+    </table>
+	
 
-                @foreach($spells as $spell)
-                    <tr>
-                        <td>{{$spell -> level}}</td>
-                        <td><a href="{{url('/api/spell/detail/' . $spell -> spell_id)}}">{{$spell -> name}}</a></td>
-                        <td>{{$spell -> formattedClasses()}}</td>
-                        <td>{{$spell -> components}}</td>
-                        <td>{{$spell -> school}}</td>
-                        <td><a href="{{url('/api/spell/' . $spell -> spell_id)}}" class = "btn btn-primary">Delete</a></td>
-                        <td style="text-align:center;">
-                            <div class="form-check" >
-                                <input type="checkbox" name="spells[]" class="form-check-input" value="{{$spell->spell_id}}" id="spell" style="width:20px;height:20px;">
-                            </div>
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
-    @include("spellbook", ["spellbook" => $spellbooks])
-</form>
+	
+</div>
 </body>
 @endsection
