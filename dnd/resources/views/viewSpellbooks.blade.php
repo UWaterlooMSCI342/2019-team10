7213ac78763d18cb8cc3a6e25dc475c65483ed44
Sunaina Basa <sbasa@uwaterloo.ca>
2019-11-18 13:25:13 -0500

    Added background to the button headers

--- dnd/resources/views/viewSpellbooks.blade.php
+++ dnd/resources/views/viewSpellbooks.blade.php
@@ -38,11 +38,11 @@ function printPage (sURL) {
     <a href="{{url('/api/spells/')}}"><h1 align = "center"><font size = "5"; color = #D30909>Dungeons & Dragons</font></h1></a>
 
 </div>
-<div>
 
-<div >
+
+<div style = "padding: 16px; width:fit-content; height: fit-content; background-color: #212429">
 <h1 style = "color: white; font-size: 18px">Current Spellbook </h1>
-</div> 
+
     <div class="btn-group">
    
         @if($selected_spellbook != null)                                    
@@ -55,20 +55,25 @@ function printPage (sURL) {
             <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Select Spellbook
             <span class = "caret"></span></button>
         @endif
+       
         <div aria-labelledby="dropdownMenuLink">
         
                 <div class="dropdown-menu" >
+                
         @foreach($spellbooks as $spellbook) 
             <a class="dropdown-item" href="{{url('api/spellbooks/' . $spellbook->spell_book_id)}}"> {{$spellbook->name}}</a>
-           
+          
         @endforeach
         </div>
     </div>
-   
+    </div> 
 		<div class="btn-group">
 		<button onclick="printPage('{{url('/api/spellbook/export/' . $selected_spellbook->spell_book_id)}}');" class="btn btn-primary">Export to Pdf </button>
 		</div>
 </div>
+<div>
+
+
 <div style="height:400px;overflow:auto;">
     <table class="table table-inverse table-dark">
         <thead>
@@ -100,12 +105,14 @@ function printPage (sURL) {
     @else
 
     </table>
+    
     <div style="display: inline-block; padding: 20px; height: 90px; width: 30%; text-align: center; background-color: #282828;">
         <font  size = "5"; color = #D30909><h4> No spells are added to this spellbook </h4></font>
     </div>
     @endif
 </div>
+
 </body>
 </html>
-
+</div>
 @endsection 
